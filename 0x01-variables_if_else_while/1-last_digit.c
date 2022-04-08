0cc4d6afdd49462614ac29cf4e10990cc51138ed
Cocorine <43640076+Cocorine@users.noreply.github.com>
2022-04-09 00:31:49 +0200

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 last = n % 10;
 if (n == 0)
 {
-printf("Last digit of %i is %i and is 0\n");
+printf("Last digit of %i is %i and is 0\n", n, last);
 }
 else if (n > 5)
 {
