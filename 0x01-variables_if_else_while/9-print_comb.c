0be4981fbf705359b4002a2b3beca5dbd06969b5
Cocorine <43640076+Cocorine@users.noreply.github.com>
2022-04-09 02:39:09 +0200

    A program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,6 +7,22 @@
  */
 int main(void)
 {
+int c = 0;
+int i;
+while (c <= 9)
+{
+i = 0;
+while (i <= 9)
+{
+if ( c != i && c < i)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+c++;
+}
 putchar('\n');
 return (0);
 }
