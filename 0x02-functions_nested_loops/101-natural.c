a76bc304a1ca5c62cb2d55cdce66222db75499b9
Cocorine <43640076+Cocorine@users.noreply.github.com>
2022-04-13 01:50:53 +0200

    Add files via upload

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,11 @@
 int main(void)
 {
 	int i, result = 0;
-	
+
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
-		{
 			result += i;
-		}
 	}
 	printf("%d\n", result);
 	return (0);
