0c6b4f2299a68c4e337b95c9399d187dde017f75
Cocorine <43640076+Cocorine@users.noreply.github.com>
2022-04-13 03:33:13 +0200

    finds and prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
- * main - Entry point
+ * main - finds and prints the sum of the even-valued terms
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
+	int f0 = 0;
+	int f1 = 1;
+	int i = 2;
+	int fn2, suite;
+
+	suite = f0 + f1;
+	f1 = f0;
+	while (suite < 4000000)
+	{
+		fn2 = suite;
+		suite = suite + f1;
+		f1 = fn2;
+		if (i % 2 == 0 && suite < 4000000)
+		{
+			if (i != 2)
+				printf(", ");
+
+			printf("%d", suite);
+
+		}
+		i++;
+	}
+	printf("\n");
 	return (0);
 }
