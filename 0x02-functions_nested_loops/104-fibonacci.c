fa7c4d2776f453ffbec1e79482ffe1e85780e1e6
Cocorine <43640076+Cocorine@users.noreply.github.com>
2022-04-13 03:49:08 +0200

    prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
- * main - Entry point
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
+	int f0 = 0;
+	long long int f1 = 1;
+	int i = 2;
+	long long int fn2, suite;
+
+	suite = f0 + f1;
+	f1 = f0;
+	while (i < 100)
+	{
+		fn2 = suite;
+		suite = suite + f1;
+		printf("%lld", suite);
+		f1 = fn2;
+		if (i < 100)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 	return (0);
 }
