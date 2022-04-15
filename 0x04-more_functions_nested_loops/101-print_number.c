11346f2d58022e2cedf4ee035659bd82d61b862c
thefuturistagency <thefuturistagency.group@gmail.com>
2022-04-15 02:44:13 +0200

    prints an integer.

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_long - prints value
+ * @value: number input
+ * Return: void (Success)
+ */
+
+void print_long(long value)
+{
+	if (value > 0)
+	{
+		print_long(value / 10);
+		_putchar((value % 10) + '0');
+	}
+}
+
+/**
+ * print_number -prints an integer.
+ * @n: int value
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+_putchar('-');
+}
+print_long(n / 10);
+_putchar((n % 10) + '0');
+}
