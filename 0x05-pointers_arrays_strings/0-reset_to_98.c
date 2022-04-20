1bb2003e8c595c9e8f7c3def97790c617f1ef758
thefuturistagency <thefuturistagency.group@gmail.com>
2022-04-20 20:08:18 +0200

    use a pointer to update the value of an integer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - use a pointer to update the value of an integer
  * @n: parameter
- * 
+ *
  * Description - reset_to_98 resets the value of *n to 98
  */
 void reset_to_98(int *n)
