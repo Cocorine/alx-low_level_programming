edf0adbce721eb81f205d5edcf13a8345d7b0675
thefuturistagency <thefuturistagency.group@gmail.com>
2022-05-05 01:35:41 +0100

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
+
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	char *p_str;
+	int i, index = 0, len = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
+	if (s2 == NULL)
+		s2 = "";
 
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
+	for (i = 0; s1[i] || s2[i]; i++)
+		len++;
 
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
+	p_str = malloc(sizeof(char) * len);
 
-	if (array == NULL)
+	if (p_str == NULL)
 		return (NULL);
 
-	while (*s1)
-	{
-		array[i] = *s1;
-		i++;
-		s1++;
-	}
+	for (i = 0; s1[i]; i++)
+		p_str[index++] = s1[i];
 
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (array);
+	for (i = 0; s2[i]; i++)
+		p_str[index++] = s2[i];
+
+	return (p_str);
 }
